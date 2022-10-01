588023fabb422a41d066d22f092561678a4d836c
Jeff Tagle <jeffreyrftagle@gmail.com>
2022-10-01 11:51:18 -0400

    Pretty Table Pokemon

--- main.py
+++ main.py
@@ -1,16 +1,27 @@
-import another_module
-print(another_module.another_variable)
+# import another_module
+# print(another_module.another_variable)
+#
+# # import turtle
+# # timmy = turtle.Turtle()
+# #(ORRR)
+# from turtle import Turtle, Screen
+# timmy = Turtle()
+# print(timmy)
+# timmy.shape("turtle")
+# timmy.color("DarkGreen")
+# timmy.forward(100)
+#
+# my_screen = Screen()
+# print(my_screen.canvheight) #object.attribute
+# my_screen.exitonclick() #object.method (this specific code ends when clicking on screen)
 
-# import turtle
-# timmy = turtle.Turtle()
-#(ORRR)
-from turtle import Turtle, Screen
-timmy = Turtle()
-print(timmy)
-timmy.shape("turtle")
-timmy.color("DarkGreen")
-timmy.forward(100)
+#pypi.org repo of soft developed and shared by the community
+#Pretty Table ASCII table format
+#Settings --> Project NAme --> Python Interpreter --> + sign --> search and install package
+import prettytable
 
-my_screen = Screen()
-print(my_screen.canvheight) #object.attribute
-my_screen.exitonclick() #object.method (this specific code ends when clicking on screen)
+from prettytable import PrettyTable
+table = PrettyTable() #Pascal casing
+table.add_column("Pokemon",["Pikachu", "Squirtle", "Charmander"])
+table.add_column("Type",["Electric", "Water", "Fire"])
+print(table)
